40732dc2e746495dd1a25a4b2b24fbe5437c14db
zacharybretton <zbretton@utexas.edu>
2021-11-19 15:34:05 -0600

    tweaks resulting from troubleshooting

---
+++ PHG_masker.py
@@ -0,0 +1,116 @@
+#Imports
+import warnings
+import sys
+if not sys.warnoptions:
+        warnings.simplefilter("ignore")
+import numpy as np
+import nibabel as nib
+import scipy as scipy
+import scipy.io as sio
+from nilearn.input_data import NiftiMasker, MultiNiftiMasker
+from scipy import stats
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import fnmatch
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import PredefinedSplit, cross_validate, cross_val_predict
+from sklearn.feature_selection import VarianceThreshold, f_classif, SelectKBest
+from sklearn.preprocessing import StandardScaler
+
+subs=['02','03','04']
+
+
+brain_flag='MNI'  #or MNI
+task_flag='preremoval' #preremoval, study, postremoval 
+
+for num in range(len(subs)):
+    sub_num=subs[num]
+    
+    #define the subject
+    sub = ('sub-0%s' % sub_num)
+    container_path='/scratch1/06873/zbretton/repclear_dataset/BIDS/derivatives/fmriprep'
+    
+    bold_path=os.path.join(container_path,sub,'func/')
+    os.chdir(bold_path)
+    
+    #set up the path to the files and then moved into that directory
+    
+    #find the proper nii.gz files
+    def find(pattern, path):
+            result = []
+            for root, dirs, files in os.walk(path):
+                    for name in files:
+                            if fnmatch.fnmatch(name, pattern):
+                                    result.append(os.path.join(root, name))
+            return result
+        
+    
+    functional_files=find('*-%s_*.nii.gz' % task_flag, bold_path)
+    wholebrain_mask_path=find('*-%s_*mask*.nii.gz' % task_flag, bold_path)
+    
+    if brain_flag=='MNI':
+        pattern = '*MNI152NLin2009cAsym*'
+        pattern2= '*MNI152NLin2009cAsym*aparcaseg*'
+        brain_mask_path = fnmatch.filter(wholebrain_mask_path,pattern)
+        functional_files = fnmatch.filter(functional_files,pattern2)
+    elif brain_flag=='T1w':
+        pattern = '*T1w*'
+        pattern2= '*T1w*aparcaseg*'
+        brain_mask_path = fnmatch.filter(wholebrain_mask_path,pattern)
+        functional_files = fnmatch.filter(functional_files,pattern2)
+    
+    subject_path=os.path.join(container_path,sub)
+    
+    def new_mask(subject_path):
+        srcdir = subject_path
+        outdir = os.path.join(subject_path, 'new_mask')
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+        aparc_aseg = functional_files[0]
+    
+        #lh_fusiform = [os.path.join(outdir,'lh_fusiform.nii.gz'),1007]
+        #rh_fusiform = [os.path.join(outdir,'rh_fusiform.nii.gz'),2007]
+
+        lh_parahippocampal_anterior = [os.path.join(outdir,'lh_parahippocampal_anterior'),1006]
+        rh_parahippocampal_anterior = [os.path.join(outdir,'rh_parahippocampal_anterior'),2006]        
+
+        lh_parahippocampal = [os.path.join(outdir,'lh_parahippocampal.nii.gz'),1016]
+        rh_parahippocampal = [os.path.join(outdir,'rh_parahippocampal.nii.gz'),2016]
+    
+        # lh_inferiortemporal = [os.path.join(outdir,'lh_inferiortemporal.nii.gz'),1009]
+        # rh_inferiortemporal = [os.path.join(outdir,'rh_inferiortemporal.nii.gz'),2009]
+    
+        # lh_lingual = [os.path.join(outdir,'lh_lingual.nii.gz'),1013]
+        # rh_lingual = [os.path.join(outdir,'rh_lingual.nii.gz'),2013]
+        
+        # lh_lateraloccipital = [os.path.join(outdir,'lh_lateraloccipital.nii.gz'),1011]
+        # rh_lateraloccipital = [os.path.join(outdir,'rh_lateraloccipital.nii.gz'),2011]
+
+        # lh_calcarine = [os.path.join(outdir,'lh_paracalarine.nii.gz'),1021]
+        # rh_calcarine = [os.path.join(outdir,'rh_paracalcarine.nii.gz'),2021]
+
+        # lh_occipital_pole = [os.path.join(outdir,'lh_occipitalpole.nii.gz'),1142]
+        # rh_occipital_pole = [os.path.join(outdir,'rh_occipitalpole.nii.gz'),2142]
+
+        # lh_temporal_pole = [os.path.join(outdir,'lh_temporalpole.nii.gz'),1033]
+        # rh_temporal_pole = [os.path.join(outdir,'rh_temporalpole.nii.gz'),2033]
+
+        # lh_middletemporal = [os.path.join(outdir,'lh_middletemporal.nii.gz'), 1015]
+        # rh_middletemporal = [os.path.join(outdir,'rh_middletemporal.nii.gz'), 2015]
+
+        # lh_superiortemporal = [os.path.join(outdir,'lh_superiortemporal.nii.gz'),1030]
+        # rh_superiortemporal = [os.path.join(outdir,'rh_superiortemporal.nii.gz'),2030]
+
+        # lh_occipitalfusiform = [os.path.join(outdir,'lh_occipitalfusiform.nii.gz'),1117]
+        # rh_occipitalfusiform = [os.path.join(outdir, 'rh_occipitalfusiform.nii.gz'), 2117]
+    
+        for roi in [lh_parahippocampal_anterior,rh_parahippocampal_anterior,lh_parahippocampal,rh_parahippocampal]:
+                os.system('fslmaths %s -thr %s -uthr %s %s'%(aparc_aseg, roi[1], roi[1], roi[0]))
+        out_mask = os.path.join(outdir, 'PHG_%s_%s_mask.nii.gz' % (task_flag,brain_flag))
+                
+        os.system('fslmaths %s -add %s -add %s -add %s -bin %s'%(lh_parahippocampal[0], rh_parahippocampal[0], lh_parahippocampal_anterior[0], rh_parahippocampal_anterior[0],out_mask))
+    new_mask(subject_path)
+    print('%s mask done' % sub)
--- confusion_matrix_maker.py
+++ confusion_matrix_maker.py
@@ -19,65 +19,124 @@ import pickle
 
 subs=['02','03','04']
 
-mask_flag = 'wholebrain' #'vtc'/'wholebrain'
-brain_flag = 'MNI'
+mask_flag = 'PHG' #'vtc'/'wholebrain'/'GM'/'GM_group'/'PHG'/'FG'
+brain_flag = 'T1w'
 TR_shift=6
-ses = 'study'
+ses = 'localizer' #study/localizer/btwnsub
+clear_data=1
+rest='on'
 
 if ses=='study':
     ses_label='operation'
 elif ses=='localizer':
     ses_label='category'
+elif ses=='btwnsub':
+    ses_label='operation'
 
 def group_cmatrix(subs):
     group_mean_confusion=[]
     oper_confusion_mean=[]
-    for num in subs:
-        print('Running sub-0%s...' % num)
-        #define the subject
-        sub = ('sub-0%s' % num)
+    if ses=='btwnsub':
+        print('Running...')
+        #define the path
         container_path='/scratch1/06873/zbretton/repclear_dataset/BIDS/derivatives/fmriprep'
-        
-      
-        bold_path=os.path.join(container_path,sub)
+        bold_path=container_path
         os.chdir(bold_path)
 
-        if ses=='study':
-            sub_dict=pickle.load(open("%s-studyoperation_%s_%s_%sTR lag_data.pkl" % (sub,brain_flag,mask_flag,TR_shift),'rb'))            
-            sub_dict_pred=sub_dict["L2 Predictions (No Rest)"]
-            sub_dict_true=sub_dict["L2 True (No Rest)"]            
-            sub_confusion_1=confusion_matrix(sub_dict_true[0],sub_dict_pred[0],normalize='true')
-            sub_confusion_2=confusion_matrix(sub_dict_true[1],sub_dict_pred[1],normalize='true')            
-            sub_confusion_3=confusion_matrix(sub_dict_true[2],sub_dict_pred[2],normalize='true')
-            sub_confusion_mean=np.mean( np.array([ sub_confusion_1, sub_confusion_2, sub_confusion_3]), axis=0 )
-   
-        else:
-            sub_dict=pickle.load(open("%s-preremoval_%s_%s_%sTR lag_data.pkl" % (sub,brain_flag,mask_flag,TR_shift),'rb'))                      
-            sub_dict_pred=sub_dict['L2 Predictions (only On)']
-            sub_dict_true=sub_dict['L2 True (only On)']
-            sub_confusion_1=confusion_matrix(sub_dict_true[0],sub_dict_pred[0],normalize='true')
-            sub_confusion_2=confusion_matrix(sub_dict_true[1],sub_dict_pred[1],normalize='true')
-            sub_confusion_mean=np.mean( np.array([ sub_confusion_1, sub_confusion_2]), axis=0 )
-        group_mean_confusion.append(sub_confusion_mean)
+        sub_dict=pickle.load(open("btwnsuboperation_%s_%s_%sTR lag_data_cleaned.pkl" % (brain_flag,mask_flag,TR_shift),'rb'))            
+        sub_dict_pred=sub_dict["L2 Predictions (No Rest)"]
+        sub_dict_true=sub_dict["L2 True (No Rest)"]            
+        sub_confusion_1=confusion_matrix(sub_dict_true[0],sub_dict_pred[0],normalize='true')
+        sub_confusion_2=confusion_matrix(sub_dict_true[1],sub_dict_pred[1],normalize='true')            
+        sub_confusion_3=confusion_matrix(sub_dict_true[2],sub_dict_pred[2],normalize='true')
+        sub_confusion_mean=np.array([ sub_confusion_1, sub_confusion_2, sub_confusion_3])    
+        group_mean_confusion=sub_confusion_mean
+    else:
+        for num in subs:
+            print('Running sub-0%s...' % num)
+            #define the subject
+            sub = ('sub-0%s' % num)
+            container_path='/scratch1/06873/zbretton/repclear_dataset/BIDS/derivatives/fmriprep'
+
+            bold_path=os.path.join(container_path,sub)
+            os.chdir(bold_path)
+
+            if ses=='study':
+                if clear_data==0:
+                    sub_dict=pickle.load(open("%s-studyoperation_%s_%s_%sTR lag_data.pkl" % (sub,brain_flag,mask_flag,TR_shift),'rb'))            
+                    sub_dict_pred=sub_dict["L2 Predictions (No Rest)"]
+                    sub_dict_true=sub_dict["L2 True (No Rest)"]            
+                    sub_confusion_1=confusion_matrix(sub_dict_true[0],sub_dict_pred[0],normalize='true')
+                    sub_confusion_2=confusion_matrix(sub_dict_true[1],sub_dict_pred[1],normalize='true')            
+                    sub_confusion_3=confusion_matrix(sub_dict_true[2],sub_dict_pred[2],normalize='true')
+                    sub_confusion_mean=np.mean( np.array([ sub_confusion_1, sub_confusion_2, sub_confusion_3]), axis=0 )
+                else:
+                    sub_dict=pickle.load(open("%s-studyoperation_%s_%s_%sTR lag_data_cleaned.pkl" % (sub,brain_flag,mask_flag,TR_shift),'rb'))            
+                    sub_dict_pred=sub_dict["L2 Predictions (No Rest)"]
+                    sub_dict_true=sub_dict["L2 True (No Rest)"]            
+                    sub_confusion_1=confusion_matrix(sub_dict_true[0],sub_dict_pred[0],normalize='true')
+                    sub_confusion_2=confusion_matrix(sub_dict_true[1],sub_dict_pred[1],normalize='true')            
+                    sub_confusion_3=confusion_matrix(sub_dict_true[2],sub_dict_pred[2],normalize='true')
+                    sub_confusion_mean=np.mean( np.array([ sub_confusion_1, sub_confusion_2, sub_confusion_3]), axis=0 )
+       
+            elif ses=='localizer':
+                if rest=='off':
+                    if clear_data==0:
+                        sub_dict=pickle.load(open("%s-preremoval_rest_%s_%s_%s_%sTR lag_data.pkl" % (sub,rest,brain_flag,mask_flag,TR_shift),'rb'))                      
+                        sub_dict_pred=sub_dict['L2 Predictions (only On)']
+                        sub_dict_true=sub_dict['L2 True (only On)']
+                        sub_confusion_1=confusion_matrix(sub_dict_true[0],sub_dict_pred[0],normalize='true')
+                        sub_confusion_2=confusion_matrix(sub_dict_true[1],sub_dict_pred[1],normalize='true')
+                        sub_confusion_mean=np.mean( np.array([ sub_confusion_1, sub_confusion_2]), axis=0 )
+                    else:
+                        sub_dict=pickle.load(open("%s-preremoval_rest_%s_%s_%s_%sTR lag_data_cleaned.pkl" % (sub,rest,brain_flag,mask_flag,TR_shift),'rb'))                      
+                        sub_dict_pred=sub_dict['L2 Predictions (only On)']
+                        sub_dict_true=sub_dict['L2 True (only On)']
+                        sub_confusion_1=confusion_matrix(sub_dict_true[0],sub_dict_pred[0],normalize='true')
+                        sub_confusion_2=confusion_matrix(sub_dict_true[1],sub_dict_pred[1],normalize='true')
+                        sub_confusion_mean=np.mean( np.array([ sub_confusion_1, sub_confusion_2]), axis=0 )     
+                elif rest=='on':
+                    if clear_data==0:
+                        sub_dict=pickle.load(open("%s-preremoval_rest_%s_%s_%s_%sTR lag_data.pkl" % (sub,rest,brain_flag,mask_flag,TR_shift),'rb'))                      
+                        sub_dict_pred=sub_dict['L2 Predictions (With Rest)']
+                        sub_dict_true=sub_dict['L2 True (With Rest)']
+                        sub_confusion_1=confusion_matrix(sub_dict_true[0],sub_dict_pred[0],normalize='true')
+                        sub_confusion_2=confusion_matrix(sub_dict_true[1],sub_dict_pred[1],normalize='true')
+                        sub_confusion_mean=np.mean( np.array([ sub_confusion_1, sub_confusion_2]), axis=0 )
+                    else:
+                        sub_dict=pickle.load(open("%s-preremoval_rest_%s_%s_%s_%sTR lag_data_cleaned.pkl" % (sub,rest,brain_flag,mask_flag,TR_shift),'rb'))                      
+                        sub_dict_pred=sub_dict['L2 Predictions (With Rest)']
+                        sub_dict_true=sub_dict['L2 True (With Rest)']
+                        sub_confusion_1=confusion_matrix(sub_dict_true[0],sub_dict_pred[0],normalize='true')
+                        sub_confusion_2=confusion_matrix(sub_dict_true[1],sub_dict_pred[1],normalize='true')
+                        sub_confusion_mean=np.mean( np.array([ sub_confusion_1, sub_confusion_2]), axis=0 )                     
+
+
+            group_mean_confusion.append(sub_confusion_mean)
     return group_mean_confusion
 
-
-#labels=['animals', 'food', 'tools', 'scenes', 'scrambled', 'rest']
-labels=['scenes','faces']
-oper_labels=['maintain', 'replace', 'suppress']
+if rest=='off':
+    labels=['scenes','faces']
+    oper_labels=['maintain', 'replace', 'suppress']
+elif rest=='on':
+    labels=['rest','scenes','faces']
+    oper_labels=['rest','maintain', 'replace', 'suppress']    
 
 fig=plt.figure()
 group_mean_confusion=group_cmatrix(subs)
 plot_confusion=np.mean(group_mean_confusion,axis=0)
 plt.style.use('fivethirtyeight')
-ax = sns.heatmap(data=(plot_confusion*100),annot=True,cmap='jet')
-if ses=='localizer':
+ax = sns.heatmap(data=(plot_confusion*100),annot=True,cmap='crest',vmin=10,vmax=90)
+if ses_label=='category':
     ax.set(xlabel='Predicted', ylabel='True', xticklabels=labels, yticklabels=labels,title='Group Mean X-Validation')
-elif ses=='study':
+elif ses_label=='operation':
     ax.set(xlabel='Predicted', ylabel='True', xticklabels=oper_labels, yticklabels=oper_labels,title='Group Mean X-Validation')
 ax.set_yticklabels(ax.get_yticklabels(), rotation=45)
 ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
 plt.tight_layout()
 os.chdir('/scratch1/06873/zbretton/repclear_dataset/BIDS/derivatives/fmriprep/figs')
-fig.savefig('%s_xvalidation_%sclassifier_%s_TR%s.png' % (ses,ses_label,brain_flag,TR_shift), dpi=fig.dpi)
+if clear_data==0:
+    fig.savefig('%s_xvalidation_rest_%s_%sclassifier_%s_TR%s_%s.png' % (ses,rest,ses_label,brain_flag,TR_shift,mask_flag), dpi=fig.dpi)
+else:
+    fig.savefig('%s_xvalidation_rest_%s_%sclassifier_%s_TR%s_%s_cleaned.png' % (ses,rest,ses_label,brain_flag,TR_shift,mask_flag), dpi=fig.dpi)
 plt.show()
