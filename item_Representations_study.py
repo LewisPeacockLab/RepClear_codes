9d057f01e2d867e28c5d8b3222ccfa539373cd5a
zacharybretton <zbretton@utexas.edu>
2021-12-10 17:09:18 -0600

    Update item_Representations_study.py

--- item_Representations_study.py
+++ item_Representations_study.py
@@ -86,7 +86,9 @@ for num in range(len(subs)):
         localizer_files = fnmatch.filter(localizer_files,pattern2)
         
     localizer_files.sort()
+    face_mask_path=os.path.join('/scratch1/06873/zbretton/repclear_dataset/BIDS/derivatives/fmriprep/group_model/group_category_lvl2/','group_face_%s_mask.nii.gz' % brain_flag)
     scene_mask_path=os.path.join('/scratch1/06873/zbretton/repclear_dataset/BIDS/derivatives/fmriprep/group_model/group_category_lvl2/','group_scene_%s_mask.nii.gz' % brain_flag)    
+    face_mask=nib.load(face_mask_path)   
     scene_mask=nib.load(scene_mask_path)
     
     #load in category mask that was created from the first GLM  
@@ -127,6 +129,8 @@ for num in range(len(subs)):
     run_list=np.concatenate((run1,run2,run3))    
     #clean data ahead of the GLM
     img_clean_scene=clean_img(img,sessions=run_list,t_r=1,detrend=False,standardize='zscore',mask_img=scene_mask,confounds=study_confounds)
+    img_clean_face=clean_img(img,sessions=run_list,t_r=1,detrend=False,standardize='zscore',mask_img=face_mask,confounds=study_confounds)
+
     '''load in the denoised bold data and events file'''
     events = pd.read_csv('/scratch1/06873/zbretton/repclear_dataset/BIDS/task-study_events.tsv',sep='\t')
     #now will need to create a loop where I iterate over the face & scene indexes
@@ -173,4 +177,31 @@ for num in range(len(subs)):
         nib.save(trial_pattern_nii, output_name)  # Save the volume  
 
         del trial_pattern, trial_pattern_nii, affine_mat, onset, out_folder, output_name, hdr
+
+    for trial in (range(len(scene_index))):
+
+        print('running scene trial %s' % (trial+1))
+        #get the onset of the trial so that I can average the time points:
+        onset=(temp_events.loc[scene_index[trial],'onset']+5)
+
+        affine_mat=img_clean_face.affine
+        dimsize = img_clean_face.header.get_zooms()
+
+        '''point to and if necessary create the output folder'''
+        out_folder = os.path.join(container_path,sub,'item_representations')
+        if not os.path.exists(out_folder): os.makedirs(out_folder,exist_ok=True)
+
+        
+        trial_pattern=np.mean(img_clean_face.get_fdata()[:,:,:,(onset):(onset+2)],axis=3) #this is getting the 2 TRs for that trial's onset and then taking the average of it across the 4th dimension (time)
+        
+
+        output_name = os.path.join(out_folder, ('Sub-0%s_study_scene_trial%s_face_masked_result.nii.gz' % (sub_num,(trial+1))))
+        trial_pattern = trial_pattern.astype('double')  # Convert the output into a precision format that can be used by other applications
+        trial_pattern[np.isnan(trial_pattern)] = 0  # Exchange nans with zero to ensure compatibility with other applications
+        trial_pattern_nii = nib.Nifti1Image(trial_pattern, affine_mat)  # create the volume image
+        hdr = trial_pattern_nii.header  # get a handle of the .nii file's header
+        hdr.set_zooms((dimsize[0], dimsize[1], dimsize[2]))
+        nib.save(trial_pattern_nii, output_name)  # Save the volume  
+
+        del trial_pattern, trial_pattern_nii, affine_mat, onset, out_folder, output_name, hdr        
         
