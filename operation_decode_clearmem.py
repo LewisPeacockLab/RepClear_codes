3a600136d14d073d293004a5275a4e9275217d85
sunaguo <sihang.guo@gmail.com>
2021-12-22 17:29:49 -0600

    updated decoding pipeline

--- operation_decode_clearmem.py
+++ operation_decode_clearmem.py
@@ -56,7 +56,7 @@ elif workspace == 'scratch':
     results_dir = '/scratch1/07365/sguo19/model_fitting_results/'
 
 
-def clearmem_cate_classification(subID="006"):
+def clearmem_cate_classification(subID="006", xval=False, C=0.01):
     # subs with study phase VTC mask: 006, 036, 077, 079
     task = "localizer"
     n_runs = 5
@@ -81,84 +81,123 @@ def clearmem_cate_classification(subID="006"):
     run_ls = Y_df["run"].values
     print(f"\nShape after subsample: X: {X.shape}, Y: {Y.shape}")
 
-    # ===== train
-    # performance eval 
-    scores = []
-    auc_scores = []
-    cms = []
-    models = []
-    fprs = []
+    if xval: 
+        print("Running xval...")
+        # ===== xval
+        # performance eval 
+        scores = []
+        auc_scores = []
+        cms = []
+        models = []
+        fprs = []
+        best_Cs = []
 
-    print("\nTraining classifier...")
-    logo = LeaveOneGroupOut()
-    for i, (train_inds, test_inds) in enumerate(logo.split(X, Y, groups=run_ls)):
-        X_train, X_test = X[train_inds], X[test_inds]
-        y_train, y_test = Y[train_inds], Y[test_inds]
-        train_groups = run_ls[train_inds]
+        print("\nTraining classifier...")
+        logo = LeaveOneGroupOut()
+        for i, (train_inds, test_inds) in enumerate(logo.split(X, Y, groups=run_ls)):
+            X_train, X_test = X[train_inds], X[test_inds]
+            y_train, y_test = Y[train_inds], Y[test_inds]
+            train_groups = run_ls[train_inds]
 
-        print(f"\nXval iter {i}, train size {X_train.shape}, test size {X_test.shape} (run {run_ls[test_inds[0]]})")
+            print(f"\nXval iter {i}, train size {X_train.shape}, test size {X_test.shape} (run {run_ls[test_inds[0]]})")
 
-        # feature selection
-        fpr = SelectFpr(f_classif, alpha=0.01).fit(X_train, y_train)
-        X_train_sub = fpr.transform(X_train)
-        X_test_sub = fpr.transform(X_test)
-        print(f"Shapes after feature selection: X_train_sub: {X_train_sub.shape}, X_test_sub: {X_test_sub.shape}")
+            # feature selection
+            fpr = SelectFpr(f_classif, alpha=0.01).fit(X_train, y_train)
+            X_train_sub = fpr.transform(X_train)
+            X_test_sub = fpr.transform(X_test)
+            print(f"Shapes after feature selection: X_train_sub: {X_train_sub.shape}, X_test_sub: {X_test_sub.shape}")
 
-        # xval training
-        parameters ={'C':[0.01,0.1,1,10,100,1000]}
-        logo_in = LeaveOneGroupOut()
-        gscv = GridSearchCV(
-            LogisticRegression(penalty='l2', solver='liblinear'), 
-            parameters,
-            cv=logo, return_train_score=True, verbose=5, n_jobs=32)
-        gscv.fit(X_train_sub, y_train, groups=train_groups)
-        model = gscv 
-        
-        # test
-        score = model.score(X_test_sub, y_test)
-        auc_score = roc_auc_score(y_test, model.predict_proba(X_test_sub), multi_class='ovr')
-        preds = model.predict(X_test_sub)
-        # confusion matrix
-        true_counts = np.asarray([np.sum(y_test == i) for i in stim_labels.keys()])
-        cm = confusion_matrix(y_test, preds, labels=list(stim_labels.keys())) / true_counts[:,None] * 100
+            # xval training
+            parameters ={'C':[0.01,0.1,1,10,100,1000]}
+            logo_in = LeaveOneGroupOut()
+            gscv = GridSearchCV(
+                LogisticRegression(penalty='l2', solver='liblinear'), 
+                parameters,
+                cv=logo, return_train_score=True, verbose=5, n_jobs=32)
+            gscv.fit(X_train_sub, y_train, groups=train_groups)
+            model = gscv 
+            
+            # test
+            score = model.score(X_test_sub, y_test)
+            auc_score = roc_auc_score(y_test, model.predict_proba(X_test_sub), multi_class='ovr')
+            preds = model.predict(X_test_sub)
+            # confusion matrix
+            true_counts = np.asarray([np.sum(y_test == i) for i in stim_labels.keys()])
+            cm = confusion_matrix(y_test, preds, labels=list(stim_labels.keys())) / true_counts[:,None] * 100
+
+            # save
+            models.append(model)
+            fprs.append(fpr)
+            scores.append(score)
+            auc_scores.append(auc_score)
+            cms.append(cm)
+            best_Cs.append(model.best_params_['C'])
+
+            if v: print(f"\nIter {i} score: \n"
+                f"score: {score}, auc score: {auc_score}\n"
+                f"confution matrix:\n"
+                f"{cm}")
+
+        scores = np.asarray(scores)
+        auc_scores = np.asarray(auc_scores)
+        cms = np.stack(cms)
+
+        print(f"\nClassifier score: \n"
+            f"scores: {scores.mean()} +/- {scores.std()}\n"
+            f"auc scores: {auc_scores.mean()} +/- {auc_scores.std()}\n"
+            f"best Cs: {best_Cs}\n"
+            f"average confution matrix:\n"
+            f"{cms.mean(axis=0)}")
 
         # save
-        models.append(model)
-        fprs.append(fpr)
-        scores.append(score)
-        auc_scores.append(auc_score)
-        cms.append(cm)
+        # out_path = os.path.join(results_dir, "category_clf", f"sub-{subID}_clearmem_task-{task}_space-{space}_VTC_gscv.npz")
+        # print(f"Saving results to {out_path}...")
+        # np.savez_compressed(out_path, models=models, fprs=fprs, scores=scores, auc_scores=auc_scores, cms=cms)
+    
+    else:
+        print("Running final model on all data...")
+        # ===== use given C value to train on all data
+        # feature selection
+        fpr = SelectFpr(f_classif, alpha=0.01).fit(X, Y)
+        X_sub = fpr.transform(X)
+        print(f"Shape after feature selection: X_sub: {X_sub.shape}")
 
-        if v: print(f"\nIter {i} score: \n"
-            f"score: {score}, auc score: {auc_score}\n"
+        lr = LogisticRegression(penalty='l2', solver='liblinear', C=C, verbose=5, n_jobs=32)
+        lr.fit(X_sub, Y)
+        model = lr 
+        
+        # test
+        score = model.score(X_sub, Y)
+        auc_score = roc_auc_score(Y, model.predict_proba(X_sub), multi_class='ovr')
+        preds = model.predict(X_sub)
+        # confusion matrix
+        true_counts = np.asarray([np.sum(Y == i) for i in stim_labels.keys()])
+        cm = confusion_matrix(Y, preds, labels=list(stim_labels.keys())) / true_counts[:,None] * 100
+
+        print(f"\nClassifier score: \n"
+            f"score: {score}\n"
+            f"auc score: {auc_score}\n"
             f"confution matrix:\n"
             f"{cm}")
 
-    scores = np.asarray(scores)
-    auc_scores = np.asarray(auc_scores)
-    cms = np.stack(cms)
-
-    print(f"\nClassifier score: \n"
-        f"scores: {scores.mean()} +/- {scores.std()}\n"
-        f"auc scores: {auc_scores.mean()} +/- {auc_scores.std()}\n"
-        f"average confution matrix:\n"
-        f"{cms.mean(axis=0)}")
-
-    # save
-    out_path = os.path.join(results_dir, "category_clf", f"sub-{subID}_clearmem_gscv.npz")
-    print(f"Saving results to {out_path}...")
-    np.savez_compressed(out_path, models=models, fprs=fprs, scores=scores, auc_scores=auc_scores, cms=cms)
-
+        # save
+        out_path = os.path.join(results_dir, "category_clf", f"sub-{subID}_clearmem_task-{task}_space-{space}_VTC_final-lr.pkl")
+        print(f"Saving results to {out_path}...")
+        # np.savez_compressed(out_path, model=model, fpr=fpr, score=score, auc_score=auc_score, cm=cm)
+        out_dict = dict(model=model, fpr=fpr, score=score, auc_score=auc_score, cm=cm)
+        with open(out_path, "wb") as f:
+            pickle.dump(out_dict, f)
+        
 
 def organize_decode_results(data, df):
     """
-    Make data (xval x TR x class) ==> (xval x trial x TR x class)
+    Make data (xval x TR x class) ==> (trial x xval x TR x class)
     df: design matrix df to organize trials
 
     Return
+    dtata_out: *LIST* of trial TRs (trial x xval x TR x class). can't make ndarray bc TR size diff.
     trial_df: original df organized into one trial per row, consistent with trial dim of returned data
     """
-    trial_ls = list(set(df["trial"]))
     # make df of trial per row
     trial_df = []
     for tid, tdf in df.groupby(["trial"]):
@@ -175,10 +214,10 @@ def organize_decode_results(data, df):
         bf_trial_inds = np.arange(trial_inds[0]-5, trial_inds[0])
         trial_inds = np.r_[bf_trial_inds, trial_inds]
 
-        trial_data = data[:, trial_inds, :]
+        trial_data = data[:, trial_inds, :]  # xval x TR x class
         data_out.append(trial_data)
-    # TODO: figure out way to stack without same dim
-    data_out = np.stack(data_out, axis=1)
+    # # TODO: figure out way to stack without same dim
+    # data_out = np.stack(data_out, axis=1)
 
     return data_out, trial_df
 
@@ -194,9 +233,12 @@ def clearmem_study_decode(subID="006"):
     # v = True
 
     # === load model
-    file_path = os.path.join(results_dir, "category_clf", f"sub-{subID}_clearmem_gscv.npz")
-    f = np.load(file_path, allow_pickle=True)
-    models, fprs = f["models"], f["fprs"]
+    file_path = os.path.join(results_dir, "category_clf", f"sub-{subID}_clearmem_task-localizer_space-{space}_VTC_final-lr.pkl")
+    # f = np.load(file_path, allow_pickle=True)
+    # model, fpr = f["model"], f["fpr"]
+    with open(file_path, "rb") as f:
+        out_dict = pickle.load(f)
+    model, fpr = out_dict["model"], out_dict["fpr"]
     
     # === load study data
     mask_path = os.path.join(clearmem_dir, f"sub-{subID}", "new_mask", f"LOC_VTC_{task}_{space}_mask.nii.gz")
@@ -204,36 +246,35 @@ def clearmem_study_decode(subID="006"):
     print("BOLD shape: ", full_data.shape)
     
     # === decode every TR
-    evids = []
-    probs = []
-    for runi, (run_model, run_fpr) in enumerate(zip(models, fprs)):
-        data_sub = run_fpr.transform(full_data)
-        # decode metrics
-        evid = (1. / (1. + np.exp(-run_model.decision_function(data_sub))))
-        prob = run_model.predict_proba(data_sub)
-
-        evids.append(evid)
-        probs.append(prob)
-
-    evids = np.stack(evids)  # xval x TR x class
-    probs = np.stack(probs)  # xval x TR x class
+    data_sub = fpr.transform(full_data)
+    # decode metrics
+    evid = (1. / (1. + np.exp(-model.decision_function(data_sub))))  # TR x class
+    prob = model.predict_proba(data_sub)
 
     # === organize into (xval x trial x TR x class)
     # load df
     label_df = get_shifted_labels("clearmem", task, shift_size_TR, rest_tag)
     print("Labels shape: ", label_df.shape)
-    evids, trial_df = organize_decode_results(evids, label_df)
-    probs, _ = organize_decode_results(probs, label_df)
+    # # make df of trial per row
+    # trial_df = []
+    # for tid, tdf in label_df.groupby(["trial"]):
+    #     trial_df.append(tdf.iloc[0])    
+    # trial_df = np.vstack(trial_df)
+    # trial_df = pd.DataFrame(trial_df, columns=label_df.columns)
 
     # === save file (can ignore df; no organizing needed, all done later in organizer)
-    out_fname = os.path.join(results_dir, "category_clf", f"sub-{subID}_decode")
+    out_fname = os.path.join(results_dir, "category_clf", f"sub-{subID}_final-lr-decode.pkl")
     print(f"Saving to {out_fname}...")
-    np.savez_compressed(evids=evids, probs=probs, trial_df=trial_df)
+    # np.savez_compressed(out_fname, evids=evids, probs=probs, label_df=label_df)
+    out_dict = dict(evid=evid, prob=prob, label_df=label_df)
+    with open(out_fname, "wb") as f:
+        pickle.dump(out_dict, f)
     
 
 if __name__ == "__main__":
-    subID = "006"
-    clearmem_cate_classification(subID)
+    subID = "079"
+    # clearmem_cate_classification(subID, xval=True)
+    # clearmem_cate_classification(subID, C=0.01)
     clearmem_study_decode(subID)
 
 
